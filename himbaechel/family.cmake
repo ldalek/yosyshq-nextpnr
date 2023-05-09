rfuest/nextpnr

himbächel: Initial skeleton
Signed-off-by: gatecat <gatecat@ds0.me>

himbaechel/family.cmake
@@ -1,0 +1,7 @@
+set(HIMBAECHEL_UARCHES "example")
+foreach(uarch ${HIMBAECHEL_UARCHES})
+    aux_source_directory(${family}/uarch/${uarch} UARCH_FILES)
+    foreach(target ${family_targets})
+        target_sources(${target} PRIVATE ${UARCH_FILES})
+    endforeach()
+endforeach(uarch)

ldalek/yosyshq-nextpnr

himbaechel/family.cmake
@@ -1,0 +1,7 @@
+set(HIMBAECHEL_UARCHES "example")
+foreach(uarch ${HIMBAECHEL_UARCHES})
+    aux_source_directory(${family}/uarch/${uarch} HM_UARCH_FILES)
+    foreach(target ${family_targets})
+        target_sources(${target} PRIVATE ${HM_UARCH_FILES})
+    endforeach()
+endforeach(uarch)